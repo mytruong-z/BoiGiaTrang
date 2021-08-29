0a6820ddd24aebabee5e23c200b8bd43915e335a
mytruong-z <my.truong@ceresolutions.com>
2021-08-30 02:42:52 +0700

    fix bug

--- server.php
+++ server.php
@@ -11,6 +11,13 @@ $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
 
+if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on') {
+    $protocol = "https://" . $_SERVER['HTTP_HOST'];
+} else {
+    $protocol = 'http://' . $_SERVER['HTTP_HOST'];
+}
+
+
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
